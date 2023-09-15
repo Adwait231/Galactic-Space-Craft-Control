690db8243061aafa02985c0967237cf59a0ccf1d
Adwait231 <AdwaitBandal231@gmail.com>
2023-09-15 13:45:41 +0530

    Fourth

--- command.py
+++ command.py
@@ -2,10 +2,22 @@ from craft_control import move
 
 coordinates = [0, 0, 0]
 direction = input("Enter initial direction of space craft: ")
-commands = input("Enter command string (f, b, l, r, u, d): ")
 
-for command in commands:
-    coordinates, direction = move(coordinates, direction, command)
+def execute(coordinates, direction, commands):
 
-print("Finl Position: ", coordinates)
-print("Final Direction: ", direction)
+    for command in commands:
+        coordinates, direction = move(coordinates, direction, command)
+
+    print("Finl Position: ", coordinates)
+    print("Final Direction: ", direction)
+
+    return coordinates,direction
+
+
+while True:
+    x = int(input("Enter 0 to exit or 1 to continue: "))
+    if(x==0):
+        break
+    else:   
+        commands = input("Enter command string (f, b, l, r, u, d): ")
+        coordinates,direction = execute(coordinates, direction, commands)
--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 import unittest
 from craft_control import move
+from command import execute
 
 class Testing(unittest.TestCase):
     def test_movement(self):
         coordinates, direction = move([0, 0, 0], 'W', 'b')
         self.assertEqual(coordinates, [1, 0, 0])
         self.assertEqual(direction,'W')
+
+    def test_execution(self):
+        coordinates, direction = execute([0,0,0], 'N', 'frubl')
+        self.assertEqual(coordinates, [0, 1, -1])
+        self.assertEqual(direction,'N')
